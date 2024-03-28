eeb5a98edf33c3d1f22b2984184c2162937315af
codeblake <codeblake@outlook.com>
2024-03-28 00:00:58 +0000

    Fixed invalid group name

--- install-artix.sh
+++ install-artix.sh
@@ -13,7 +13,7 @@ swap_size=auto
 timezone=Europe/London
 locale=en_GB
 user=blake
-user_groups=wheel,video,audio,input,seatd
+user_groups=wheel,video,audio,input,seat
 hostname=artix
 
 arch_support=true
